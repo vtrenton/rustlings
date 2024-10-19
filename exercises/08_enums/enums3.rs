6a2eab48f4e0574c071d1524364ce7eed0f82f29
Trenton VanderWert <trenton.vanderwert@gmail.com>
2024-10-19 16:12:26 -0500

    completed ex10
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -5,6 +5,14 @@ struct Point {
 
 enum Message {
     // TODO: Implement the message variant types based on their usage below.
+    Resize {
+        width: u64,
+        height: u64,
+    },
+    Move(Point),
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit,
 }
 
 struct State {
@@ -42,6 +50,13 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+        match message {
+            Message::Resize {width, height} => self.resize(width, height),
+            Message::Move(point) => self.move_position(point),
+            Message::Echo(msg) => self.echo(msg),
+            Message::ChangeColor(r, g, b) => self.change_color(r, g, b),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
