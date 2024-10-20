642a8bbf8e6810a7cb54dd963665f32e615b26bf
Trenton VanderWert <trenton.vanderwert@gmail.com>
2024-10-20 16:15:25 -0500

    onto error handling
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- exercises/13_error_handling/errors1.rs
+++ exercises/13_error_handling/errors1.rs
@@ -4,12 +4,11 @@
 // construct to `Option` that can be used to express error conditions. Change
 // the function signature and body to return `Result<String, String>` instead
 // of `Option<String>`.
-fn generate_nametag_text(name: String) -> Option<String> {
-    if name.is_empty() {
+fn generate_nametag_text(name: String) -> Result<String, String> {
+    match Some(name) {
         // Empty names aren't allowed
-        None
-    } else {
-        Some(format!("Hi! My name is {name}"))
+        None => Err("Empty names aren't allowed".to_string()),
+        Some(name) => Ok(format!("Hi! My name is {name}")),
     }
 }
 
