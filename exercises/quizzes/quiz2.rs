9398c618a950821b4ef0cd0e6e8cfe4ad3a64e56
Trenton VanderWert <trenton.vanderwert@gmail.com>
2024-10-20 03:04:58 -0500

    completed second quiz
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,23 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output = Vec::new();
+        for mut tup in input {
+           let out = match tup.1 {
+                Command::Uppercase => { tup.0.to_uppercase() },
+                Command::Trim => { tup.0.trim().to_string() },
+                Command::Append(n) => {
+                    for _ in 0..n {
+                        tup.0.push_str("bar");
+                    }
+                    tup.0
+                },
+            };
+            output.push(out);
+        };
+        output
+    }
 }
 
 fn main() {
@@ -37,7 +53,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use crate::my_module::transformer;
     use super::Command;
 
     #[test]
