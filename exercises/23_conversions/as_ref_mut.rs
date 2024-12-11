0b345d1cc3937ec28c1c6e343f5a33c288000ad4
Trenton VanderWert <trenton.vanderwert@gmail.com>
2024-12-10 22:47:34 -0600

    working on last challenge
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- exercises/23_conversions/as_ref_mut.rs
+++ exercises/23_conversions/as_ref_mut.rs
@@ -2,15 +2,17 @@
 // about them at https://doc.rust-lang.org/std/convert/trait.AsRef.html and
 // https://doc.rust-lang.org/std/convert/trait.AsMut.html, respectively.
 
+use std::os::unix::ffi::OsStrExt;
+
 // Obtain the number of bytes (not characters) in the given argument.
 // TODO: Add the `AsRef` trait appropriately as a trait bound.
-fn byte_counter<T>(arg: T) -> usize {
+fn byte_counter<T: AsRef<T> + OsStrExt>(arg: T) -> usize {
     arg.as_ref().as_bytes().len()
 }
 
 // Obtain the number of characters (not bytes) in the given argument.
 // TODO: Add the `AsRef` trait appropriately as a trait bound.
-fn char_counter<T>(arg: T) -> usize {
+fn char_counter<T: AsRef<T> + OsStrExt>(arg: T) -> usize {
     arg.as_ref().chars().count()
 }
 
